f3455622dd00f833cbd113435ccbb27f33dcc361
robin <robin@robinkirkman.com>
2014-01-14 08:19:52 -0500

    More testing

--- sbnemu-core/src/main/java/org/sbnemu/core/dev/DefaultBus.java
+++ sbnemu-core/src/main/java/org/sbnemu/core/dev/DefaultBus.java
@@ -1,17 +1,25 @@
 package org.sbnemu.core.dev;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.NavigableMap;
+import java.util.Set;
 import java.util.TreeMap;
 
+import org.sbnemu.core.AddressEvent;
+import org.sbnemu.core.AddressListener;
 import org.sbnemu.core.AddressRange;
 import org.sbnemu.core.Bus;
 import org.sbnemu.core.Device;
 import org.sbnemu.core.Memory;
 import org.sbnemu.core.ex.IllegalAddressException;
 
-public class DefaultBus extends AbstractDevice implements Bus {
+public class DefaultBus extends AbstractDevice implements Bus, AddressListener {
 
 	protected NavigableMap<AddressRange, Device> devices = new TreeMap<AddressRange, Device>();
 	protected Memory defaultMemory;
@@ -50,6 +58,7 @@ public class DefaultBus extends AbstractDevice implements Bus {
 		for(AddressRange r : device.getAddresses())
 			devices.put(r, device);
 		super.setAddresses(devices.keySet().toArray(new AddressRange[0]));
+		device.addAddressListener(this);
 	}
 	public void removeDevice(Device device) {
 		Iterator<Map.Entry<AddressRange, Device>> di = devices.entrySet().iterator();
@@ -59,12 +68,36 @@ public class DefaultBus extends AbstractDevice implements Bus {
 				di.remove();
 		}
 		super.setAddresses(devices.keySet().toArray(new AddressRange[0]));
+		device.removeAddressListener(this);
 	}
 	public Memory getDefaultMemory() {
 		return defaultMemory;
 	}
 	public void setDefaultMemory(Memory defaultMemory) {
 		this.defaultMemory = defaultMemory;
+	}
+
+	protected AddressRange[] computeAddresses() {
+		Set<Device> dvs = new HashSet<Device>(devices.values());
+		List<AddressRange> computed = new ArrayList<AddressRange>();
+		for(Device d : dvs)
+			computed.addAll(Arrays.asList(d.getAddresses()));
+		Collections.sort(computed);
+		return computed.toArray(new AddressRange[0]);
+	}
+
+	protected void remapDevices() {
+		Set<Device> dvs = new HashSet<Device>(devices.values());
+		devices.clear();
+		for(Device d : dvs) {
+			for(AddressRange r : d.getAddresses())
+				devices.put(r, d);
+		}
+		super.setAddresses(devices.keySet().toArray(new AddressRange[0]));
+	}
+	
+	public void addressesChanged(AddressEvent e) {
+		remapDevices();
 	}
 	
 }
--- sbnemu-core/src/test/java/org/sbnemu/core/dev/DefaultBusTest.java
+++ sbnemu-core/src/test/java/org/sbnemu/core/dev/DefaultBusTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import org.sbnemu.core.AddressRange;
 import org.sbnemu.core.Bus;
 import org.sbnemu.core.Device;
+import org.sbnemu.core.ex.IllegalAddressException;
 
 public class DefaultBusTest {
 	private Bus bus;
@@ -42,5 +43,64 @@ public class DefaultBusTest {
 		Collections.sort(act);
 		
 		Assert.assertEquals(exp, act);
+	}
+	
+	@Test
+	public void testValidAddress() {
+		Device d = new ArrayDevice(256);
+		d.setAddresses(new AddressRange(0, 256));
+		bus.addDevice(d);
+		
+		Assert.assertEquals(0, bus.get(127));
+		try {
+			bus.set(127, 123);
+		} catch(IllegalAddressException iae) {
+			Assert.fail();
+		}
+		Assert.assertEquals(123, bus.get(127));
+	}
+	
+	@Test
+	public void testInvalidAddress() {
+		Device d = new ArrayDevice(256);
+		d.setAddresses(new AddressRange(0, 256));
+		bus.addDevice(d);
+	}
+	
+	@Test
+	public void testOverlap() {
+		Device d = new ArrayDevice(256);
+		d.setAddresses(new AddressRange(0, 256));
+		bus.addDevice(d);
+		
+		d = new ArrayDevice(256);
+		d.setAddresses(new AddressRange(128, 384));
+		try {
+			bus.addDevice(d);
+			Assert.fail();
+		} catch(IllegalAddressException iae) {
+			// expected
+		}
+	}
+	
+	@Test
+	public void testNoDefaultMemory() {
+		try {
+			bus.get(0);
+			Assert.fail();
+		} catch(IllegalAddressException iae) {
+			// expected
+		}
+	}
+	
+	@Test
+	public void testDefaultMemory() {
+		Device d = new ArrayDevice(256);
+		d.setAddresses(new AddressRange(-128, 128));
+		bus.setDefaultMemory(d);
+		
+		bus.get(0);
+		bus.set(0, 123);
+		Assert.assertEquals(123, bus.get(0));
 	}
 }
