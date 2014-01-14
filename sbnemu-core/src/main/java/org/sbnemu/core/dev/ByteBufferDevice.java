cdbbfdf22670ef4b591529dc326388da7f2b5d7f
robin <robin@robinkirkman.com>
2014-01-14 14:02:01 -0500

    Commenting

--- sbnemu-core/src/main/java/org/sbnemu/core/dev/ByteBufferDevice.java
+++ sbnemu-core/src/main/java/org/sbnemu/core/dev/ByteBufferDevice.java
@@ -4,13 +4,24 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 import org.sbnemu.core.AddressRange;
+import org.sbnemu.core.Device;
 import org.sbnemu.core.ex.IllegalAddressException;
 
+/**
+ * {@link Device} similar to {@link ArrayDevice} but backed by a {@link ByteBuffer}.
+ * Because it is backed by a {@link ByteBuffer}, all values set on this device are
+ * truncated to 1 byte.<p>
+ * 
+ * {@link ByteBufferDevice} supports only a single {@link AddressRange}.
+ * @author robin
+ *
+ */
 public class ByteBufferDevice extends AbstractDevice {
 
+	/**
+	 * The byte buffer
+	 */
 	protected ByteBuffer buf;
-	protected int position;
-	protected int limit;
 	
 	// used only for deserialization
 	@Deprecated
@@ -18,33 +29,39 @@ public class ByteBufferDevice extends AbstractDevice {
 		
 	}
 	
+	/**
+	 * Wrap a {@link ByteBuffer} as the device.  The offset of memory inside
+	 * the {@link ByteBuffer} is {@link ByteBuffer#position()}, and the size 
+	 * is limited by {@link ByteBuffer#limit()}.
+	 * @param buf
+	 */
 	public ByteBufferDevice(ByteBuffer buf) {
-		this(buf, buf.position(), buf.limit());
-	}
-	
-	public ByteBufferDevice(ByteBuffer buf, int position, int limit) {
 		this.buf = buf;
-		buf.position(position);
-		buf.limit(limit);
 	}
 	
 	public void setAddresses(AddressRange... addresses) {
-		if(addresses.length != 1)
+		if(addresses.length != 1) // supports only one address range
 			throw new IllegalAddressException();
-		if(addresses[0].getExtent() > buf.limit() - buf.position())
+		if(addresses[0].getExtent() > buf.limit() - buf.position()) // ensure range isn't too large
 			throw new IllegalAddressException();
-		if(buf == null) {
+		if(buf == null) { // create buffer if null, such as when deserializing
 			buf = ByteBuffer.allocate((int)addresses[0].getExtent());
-			position = buf.position();
-			limit = buf.limit();
 		}
 		super.setAddresses(addresses);
 	}
 	
+	/**
+	 * Return the base address of the single {@link AddressRange}
+	 * @return
+	 */
 	protected long getBaseAddress() {
 		return getAddresses()[0].getBaseAddress();
 	}
 	
+	/**
+	 * Return the end address of the single {@link AddressRange}
+	 * @return
+	 */
 	protected long getEndAddress() {
 		return getAddresses()[0].getEndAddress();
 	}
@@ -55,7 +72,7 @@ public class ByteBufferDevice extends AbstractDevice {
 	}
 	
 	public void set(long address, long value) {
-		validateAddress(address);;
+		validateAddress(address);
 		buf.put(buf.position() + (int)(address - getBaseAddress()), (byte) value);
 	}
 
